ea8efb44354c32e4963adb9f0bc15495bbc73127
Guus Ekkelenkamp <guus.ekkelenkamp@harborn.com>
2026-05-19 11:54:40 +0200

    feat: wire browser history import use cases into container

--- src/application/container.ts
+++ src/application/container.ts
@@ -1,19 +1,29 @@
 import { KeychainRepository } from '../infrastructure/keychain/KeychainRepository'
 import { SimplicateRepository } from '../infrastructure/simplicate/SimplicateRepository'
 import { TemplateStorageRepository } from '../infrastructure/storage/TemplateStorageRepository'
+import { MappingCacheRepository } from '../infrastructure/storage/MappingCacheRepository'
+import { CopilotRepository } from '../infrastructure/copilot/CopilotRepository'
 import { BookTemplateUseCase } from '../domain/usecases/BookTemplateUseCase'
 import { DeleteTemplateUseCase } from '../domain/usecases/DeleteTemplateUseCase'
 import { FetchSimplicateDataUseCase } from '../domain/usecases/FetchSimplicateDataUseCase'
 import { SaveTemplateUseCase } from '../domain/usecases/SaveTemplateUseCase'
+import { ParseBrowserHistoryUseCase } from '../domain/usecases/ParseBrowserHistoryUseCase'
+import { ClassifyHistoryBlocksUseCase } from '../domain/usecases/ClassifyHistoryBlocksUseCase'
 import type { ISimplicateRepository } from '../domain/repositories/ISimplicateRepository'
+import type { ICopilotRepository } from '../domain/repositories/ICopilotRepository'
 
 // Repositories
 export const keychainRepo = new KeychainRepository()
 export const templateRepo = new TemplateStorageRepository()
+export const mappingCacheRepo = new MappingCacheRepository()
 
 // SimplicateRepository is created lazily after credentials are loaded
 export function createSimplicateRepository(baseUrl: string, apiKey: string, apiSecret: string) {
   return new SimplicateRepository(baseUrl, apiKey, apiSecret)
+}
+
+export function createCopilotRepository(token: string): ICopilotRepository {
+  return new CopilotRepository(token)
 }
 
 // Use cases (stateless, created with injected repos)
@@ -23,5 +33,8 @@ export function createUseCases(simplicateRepo: ISimplicateRepository) {
     deleteTemplate: new DeleteTemplateUseCase(templateRepo),
     bookTemplate: new BookTemplateUseCase(simplicateRepo),
     fetchSimplicateData: new FetchSimplicateDataUseCase(simplicateRepo),
+    parseBrowserHistory: new ParseBrowserHistoryUseCase(),
+    classifyHistoryBlocks: (copilotRepo: ICopilotRepository) =>
+      new ClassifyHistoryBlocksUseCase(copilotRepo, mappingCacheRepo),
   }
 }
