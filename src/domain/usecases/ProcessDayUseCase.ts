848ab8a06c504f00c3946e24cf932d850d753a15
Guus Ekkelenkamp <guus.ekkelenkamp@harborn.com>
2026-05-24 11:16:59 +0200

    feat: ProcessDayUseCase uses GetActiveProjectsForDateUseCase for filtered project context

--- src/domain/usecases/ProcessDayUseCase.ts
+++ src/domain/usecases/ProcessDayUseCase.ts
@@ -5,9 +5,11 @@ import type { IGoogleCalendarRepository } from '../repositories/IGoogleCalendarR
 import type { IHistoryStore } from '../repositories/IHistoryStore'
 import type { ICopilotRepository, Project, Service } from '../repositories/ICopilotRepository'
 import type { IMappingCacheRepository } from '../repositories/IMappingCacheRepository'
+import type { ISimplicateRepository } from '../repositories/ISimplicateRepository'
 import { FetchGitHubContextUseCase } from './FetchGitHubContextUseCase'
 import { FetchLinearContextUseCase } from './FetchLinearContextUseCase'
 import { GroupAndClassifyDayUseCase } from './GroupAndClassifyDayUseCase'
+import { GetActiveProjectsForDateUseCase } from './GetActiveProjectsForDateUseCase'
 import { groupCommitsIntoBlocks } from './GroupCommitsIntoBlocks'
 
 export interface ProcessDayProgress {
@@ -19,6 +21,7 @@ export interface ProcessDayProgress {
 export class ProcessDayUseCase {
   private readonly fetchGitHub: FetchGitHubContextUseCase
   private readonly fetchLinear: FetchLinearContextUseCase
+  private readonly getActiveProjects: GetActiveProjectsForDateUseCase
 
   constructor(
     githubRepo: IGitHubRepository,
@@ -27,12 +30,15 @@ export class ProcessDayUseCase {
     private readonly historyStore: IHistoryStore,
     private readonly copilotRepo: ICopilotRepository,
     private readonly cacheRepo: IMappingCacheRepository,
-    private readonly availableProjects: Project[],
+    _availableProjects: Project[], // kept for call-site compat; active projects are fetched at runtime via getActiveProjects
     private readonly availableServices: Service[],
     private readonly githubUsername: string,
+    simplicateRepo: ISimplicateRepository,
+    private readonly simplicateEmployeeId: string,
   ) {
     this.fetchGitHub = new FetchGitHubContextUseCase(githubRepo)
     this.fetchLinear = new FetchLinearContextUseCase(linearRepo)
+    this.getActiveProjects = new GetActiveProjectsForDateUseCase(simplicateRepo)
   }
 
   async *execute(date: string): AsyncGenerator<ProcessDayProgress> {
@@ -42,11 +48,12 @@ export class ProcessDayUseCase {
       const dayStart = new Date(date + 'T00:00:00')
       const dayEnd = new Date(date + 'T23:59:59')
 
-      const [allCommits, linearIssues, calendarEvents, historyBlocks] = await Promise.all([
+      const [allCommits, linearIssues, calendarEvents, historyBlocks, activeProjectsResult] = await Promise.all([
         this.fetchGitHub.execute(this.githubUsername, date, date),
         this.fetchLinear.execute(date, date),
         this.calendarRepo.fetchEvents(dayStart, dayEnd),
         this.historyStore.getBlocksForDate(date),
+        this.getActiveProjects.execute(date, this.simplicateEmployeeId),
       ])
 
       yield { phase: 'classifying-day', date }
@@ -57,8 +64,9 @@ export class ProcessDayUseCase {
       const groupAndClassify = new GroupAndClassifyDayUseCase(
         this.copilotRepo,
         this.cacheRepo,
-        this.availableProjects,
+        activeProjectsResult.activeProjects,
         this.availableServices,
+        activeProjectsResult.historicalEntries,
       )
 
       const classified = await groupAndClassify.execute(date, allBlocks, calendarEvents, {
